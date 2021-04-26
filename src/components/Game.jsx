080dd1a5e7ab85a7c19d36e1853db364d3bec4c4
Ikram <jadisnou@gmail.Com>
2021-04-26 16:34:53 +0200

    Modif components

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,7 +6,8 @@ class Game extends React.Component {
       <div className='game'>
         <h1>Black Jack</h1>
         <div>
-          <button type="button" className="btn btn-outline-warning rounded-pill" style={{ height: '100px', width: '200px', fontSize: '2em' }} onClick={this.props.startGame}>Start</button>
+          <button type="button" className="btn btn-outline-warning rounded-pill" 
+          style={{ height: '100px', width: '200px', fontSize: '2em' }} onClick={this.props.startGame}>Start</button>
         </div>
       </div>
     );
