7856a7d4f873b7a94bc03e0951c795d78c72e570
Ikram <jadisnou@gmail.Com>
2021-04-27 12:26:24 +0200

    changements non fini

--- src/Table.js
+++ src/Table.js
@@ -4,16 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from './components/Button.jsx'
 import Cartes from "./components/Cartes.jsx";
 import StartGame from './components/StartGame.jsx'
+import cardUtils from './utils/cardUtils'
 
-const cardArray = [
-  "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
-  "KD", "QD", "JD", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "0D",
-  "KH", "QH", "JH", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "0H",
-  "KC", "QC", "JC", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "0C"];
+// const cardArray = [
+//   "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
+//   "KD", "QD", "JD", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "0D",
+//   "KH", "QH", "JH", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "0H",
+//   "KC", "QC", "JC", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "0C"];
 
 
-let rndCarteTemp = "";
-let rndNumTemp = 0;
+// let rndCarteTemp = "";
+// let rndNumTemp = 0;
 
 class Table extends React.Component {
   constructor() {
@@ -29,19 +30,20 @@ class Table extends React.Component {
       endGame: false,
       nameOfWinner: ""
     }
+    this.renderGame = this.renderGame.bind(this)
   }
   // Distribution aléatoire de cartes entre 0 à 53
-  rndCarte() {
-
-    rndNumTemp = Math.floor(Math.random() * 53);
-
-    if (rndNumTemp > 52) { rndNumTemp = rndNumTemp - 10 } else if (rndNumTemp < 1) { rndNumTemp = rndNumTemp + 10 }
-
-    rndCarteTemp = cardArray[rndNumTemp - 1];
-
-    return rndCarteTemp
-  }
-  // 
+  // rndCarte() {
+    
+    //   rndNumTemp = Math.floor(Math.random() * 53);
+    
+    //   if (rndNumTemp > 52) { rndNumTemp = rndNumTemp - 10 } else if (rndNumTemp < 1) { rndNumTemp = rndNumTemp + 10 }
+    
+    //   rndCarteTemp = cardArray[rndNumTemp - 1];
+    
+    //   return rndCarteTemp
+    // }
+    // 
   onClickStop = () => {
     const cardSelectedDealer = this.rndCarte()
     const cardSelectedDealer2 = this.rndCarte()
@@ -141,12 +143,11 @@ class Table extends React.Component {
       playerCardList: firstTwoCardsPlayer,
       startGame: true
     })
-    this.renderGame = this.renderGame.bind(this)
   }
 
   renderGame() {
     return(
-    <div>
+      <div>
       <div className="playGame">
         <div style={{ height: '100vh', position: 'relative' }}>
           <h1 style={{ color: '#feb236', textAlign: 'center' }}>Black Jack</h1>
@@ -165,7 +166,7 @@ class Table extends React.Component {
                 color="white"
                 bcolor="#0d6efd"
                 name="Give"
-              />
+                />
             </div>
             <div>
             </div>
@@ -177,7 +178,8 @@ class Table extends React.Component {
                 color="white"
                 bcolor="#dc3545"
                 name="Stop"
-              />
+                />
+              <cardUtils></cardUtils>
             </div>
           </div>
         </div>
